4ffe2400c72d9b3f9edceeb0d6c1aeb6478ef72a
James Chiella <james.chiella@gmail.com>
2022-08-28 22:27:21 -0400

    implemented basic screen with graphics test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Tortilla!");
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use winit_input_helper::WinitInputHelper;
+
+fn main() -> Result<(), Error> {
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(64 * 16, 32 * 16);
+        WindowBuilder::new()
+            .with_title("Tortilla")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(64, 32, surface_texture)?
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            let frame = pixels.get_frame();
+            let blue = [0x0, 0x0, 0xff, 0xff];
+            let white = [0xff, 0xff, 0xff, 0xff];
+
+            for pixel in frame.chunks_exact_mut(4) {
+                pixel.copy_from_slice(&blue);
+            }
+
+            frame[0..4].copy_from_slice(&white);
+
+            if pixels
+                .render()
+                .map_err(|e| println!("Erorr on pixels.render(): {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        if input.update(&event) {
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+
+            window.request_redraw();
+        }
+    });
 }
