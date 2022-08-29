104e6625939e8a1082dc025b5490c9db9166a7ab
James Chiella <james.chiella@gmail.com>
2022-08-29 12:03:56 -0400

    setup fetch-decode-execute loop framework

--- src/machine.rs
+++ src/machine.rs
@@ -1,12 +1,23 @@
-struct CHIP8 {
-    memory: [u16; 4096],
-    stack: [u16; 16],
+const MEMORY_SIZE: usize = 4096;
+const STACK_SIZE: usize = 16;
+const NUM_REGISTERS: usize = 16;
+pub const CLOCK_SPEED: u64 = 700; // Hz (instructions/second)
+pub const SCREEN_SIZE_ROWS: u32 = 64;
+pub const SCREEN_SIZE_COLS: u32 = 32;
+
+pub struct CHIP8 {
+    memory: [u16; MEMORY_SIZE],
+    screen: [bool; (SCREEN_SIZE_ROWS * SCREEN_SIZE_COLS) as usize],
+    pc: u16,
+    i: u16,
+    stack: [u16; STACK_SIZE],
     delay_timer: u8,
     sound_timer: u8,
+    registers: [u8; NUM_REGISTERS],
 }
 
 impl CHIP8 {
-    fn new() -> Self {
+    pub fn new() -> Self {
         let font = [
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
             0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -27,14 +38,53 @@ impl CHIP8 {
         ];
 
         let mut chip8 = CHIP8 {
-            memory: [0; 4096],
-            stack: [0; 16],
+            memory: [0; MEMORY_SIZE],
+            screen: [false; (SCREEN_SIZE_ROWS * SCREEN_SIZE_COLS) as usize],
+            pc: 0x0200,
+            i: 0,
+            stack: [0; STACK_SIZE],
             delay_timer: 0,
             sound_timer: 0,
+            registers: [0; NUM_REGISTERS],
         };
 
         chip8.memory[0x0050..0x00A0].copy_from_slice(&font);
 
         chip8
+    }
+
+    pub fn fetch(&mut self) -> u16 {
+        let instruction = self.memory[self.pc as usize] | (self.memory[self.pc as usize + 1] << 8);
+        self.pc += 2;
+        instruction
+    }
+
+    pub fn decode_and_execute(&mut self, instruction: u16) {
+        let opcode = instruction & 0xf000 >> 12;
+        let x = instruction & 0x0f00 >> 8;
+        let y = instruction & 0x00f0 >> 4;
+        let n = instruction & 0x000f;
+        let nn = instruction & 0x0ff0 >> 4;
+        let nnn = instruction & 0x0fff;
+
+        match opcode {
+            0x0 => todo!(),
+            0x1 => todo!(),
+            0x2 => todo!(),
+            0x3 => todo!(),
+            0x4 => todo!(),
+            0x5 => todo!(),
+            0x6 => todo!(),
+            0x7 => todo!(),
+            0x8 => todo!(),
+            0x9 => todo!(),
+            0xA => todo!(),
+            0xB => todo!(),
+            0xC => todo!(),
+            0xD => todo!(),
+            0xE => todo!(),
+            0xF => todo!(),
+            _ => unreachable!(),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,16 @@ use winit::{
 };
 
 fn main() -> Result<(), Error> {
-    let timer_length = Duration::new(0, 16_600_000);
+    let machine = machine::CHIP8::new();
+
+    let timer_length = Duration::new(1 / machine::CLOCK_SPEED, 0);
 
     let event_loop = EventLoop::new();
     let window = {
-        let size = LogicalSize::new(64 * 16, 32 * 16);
+        let size = LogicalSize::new(
+            machine::SCREEN_SIZE_ROWS * 16,
+            machine::SCREEN_SIZE_COLS * 16,
+        );
         WindowBuilder::new()
             .with_title("Tortilla")
             .with_inner_size(size)
@@ -28,7 +33,11 @@ fn main() -> Result<(), Error> {
     let mut pixels = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(64, 32, surface_texture)?
+        Pixels::new(
+            machine::SCREEN_SIZE_ROWS,
+            machine::SCREEN_SIZE_COLS,
+            surface_texture,
+        )?
     };
 
     event_loop.run(move |event, _, control_flow| match event {
@@ -47,6 +56,8 @@ fn main() -> Result<(), Error> {
         Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
             control_flow.set_wait_until(Instant::now() + timer_length);
             // main interpreter loop goes here
+            let instruction = machine.fetch();
+            machine.decode_and_execute(instruction);
         }
         Event::WindowEvent {
             event: WindowEvent::CloseRequested,
